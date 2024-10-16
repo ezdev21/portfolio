d2c2d1466e8a7e75984747a519570dd1ca663da2
ezdev21 <ezradev21@gmail.com>
2024-10-16 21:08:51 +0300

    update

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -20,32 +20,19 @@ function Home2() {
 
             <p className="home-about-body">
               <p>
-              Hey there! 👋 I'm a super versatile software engineer who's all about crafting web and mobile apps that push the boundaries of what's possible. I live for innovation, especially when it comes to programming tech - you know, that whole internet thing? 🌐
+              Hey there! 👋 I'm a super versatile software engineer who's all about crafting web and mobile apps that push the boundaries of what's possible.
+              <br/>
+              <br/>
+              More than 3 years of working  experience on real-life projects to build web applications for businesses using Laravel.
+              <br/>
+              <br/>
+              Primarily focusing on web development, I am skilled in Laravel,Vue and React. In addition to web development, I have a brief experience working with cross-platform technologies, Flutter and Kotlin.
+              <br/>
+              <br/>
+              As a team player,  I am great at using version control systems and CI/CD tools to collaborate with teammates on projects. Well-familiar with agile Software Development methodology and modern project management tools and technologies
               </p>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Websites </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Mobile Apps
-                </b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Laravel</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Vue.js</b>
-              </i>
             </p>
           </Col>
           <Col md={4} className="myAvtar">
