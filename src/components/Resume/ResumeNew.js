c0cb585fac93d31190501bc52ead0d3f79182061
ezdev21 <ezradev21@gmail.com>
2025-09-26 18:30:44 -0700

    resume bug

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -23,6 +23,7 @@ function ResumeNew() {
           <Document file={pdf}>
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
             <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
+            <Page pageNumber={3} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
           </div>
         </Row>
