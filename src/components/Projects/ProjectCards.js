20327682dbbcbcb819fedde22e9e4ade0bef5f40
ezdev21 <ezradev21@gmail.com>
2025-10-20 16:25:28 -0700

    project link

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -13,10 +13,15 @@ function ProjectCards(props) {
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
+
+        {/* If the component contains Github link and if it's not a Blog then, it will render the below component  */}
+        {!props.isBlog && props.ghLink && (
         <Button variant="primary" href={props.ghLink} target="_blank">
           <BsGithub /> &nbsp;
           {props.isBlog ? "Blog" : "GitHub"}
         </Button>
+        )}
+
         {"\n"}
         {"\n"}
 
