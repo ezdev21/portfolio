d65651379a8fcba2c35a8fdada4c829962ef5b04
ezdev21 <ezradev21@gmail.com>
2025-10-20 21:22:08 -0700

    gimp

--- src/components/Projects/Gimp.js
+++ src/components/Projects/Gimp.js
@@ -1,15 +1,125 @@
 import { Container } from "react-bootstrap";
 import Particle from "../Particle";
 
+import img3DEffects from "../../Assets/gimp/3D-effects.jpg";
+import imgGlow from "../../Assets/gimp/anter-glow-original.webp";
+import imgAntler from "../../Assets/gimp/antler-glow.png";
+import imgSmith from "../../Assets/gimp/big-smith-and-saka.png";
+import imgBrushed from "../../Assets/gimp/brushed-steel.png";
+import imgBulb from "../../Assets/gimp/bulb-glow.jpg";
+import imgCircuit from "../../Assets/gimp/circuit.png";
+import imgSKin from "../../Assets/gimp/cracked-skin.jpeg";
+import imgCubic from "../../Assets/gimp/cubic-typography.png";
+import imgCurves from "../../Assets/gimp/curve.png";
+import imgCylinderical from "../../Assets/gimp/cylinderical-text.jpg";
+import imgDayToNight from "../../Assets/gimp/day-to-night-glow.jpg";
+import imgDoubleExposure from "../../Assets/gimp/double-exposure.png";
+import imgDualLight from "../../Assets/gimp/dual-light.png";
+import imgDuotone from "../../Assets/gimp/duotone-effect.jpg";
+import imgExplode from "../../Assets/gimp/explode.jpeg";
+import imgFaceFlag from "../../Assets/gimp/face-flag.jpg";
+import imgFaceSwap from "../../Assets/gimp/face-swap.jpg";
+import imgFinal from "../../Assets/gimp/final.jpeg";
+import imgFoggyWindow from "../../Assets/gimp/foggy-window.png";
+import imgGacLogo from "../../Assets/gimp/gac-logo.png";
+import imgGlassSphere from "../../Assets/gimp/glass-sphere-effect.png";
+import imgGlitch from "../../Assets/gimp/glitch.jpg";
+import imgGlossy from "../../Assets/gimp/glossy.jpg";
+import imgGoldenEffect from "../../Assets/gimp/golden-effect.jpg";
+import imgHandToLogo from "../../Assets/gimp/hand-to-logo.png";
+import imgHeartCloud from "../../Assets/gimp/heart-cloud.jpg";
+import imgInterlocking from "../../Assets/gimp/interlocking-letter.png";
+import imgLiquidDispersion from "../../Assets/gimp/liquid-dispersion.png";
+import imgLogoHigh from "../../Assets/gimp/logo-high-resolution.jpg";
+import imgMosaic from "../../Assets/gimp/mosaic-background.png";
+import imgOrangeGirl from "../../Assets/gimp/orange-girl.jpg";
+import imgOrthon from "../../Assets/gimp/orton.jpg";
+import imgPGSLogo from "../../Assets/gimp/pgs-logo.png";
+import imgNeon from "../../Assets/gimp/photo-neon-glow.jpg";
+import imgPhoto from "../../Assets/gimp/photo_2024-07-04_14-12-41.jpg";
+import imgPoolLOgo from "../../Assets/gimp/pool-logo.png";
+import imgPopOut from "../../Assets/gimp/pop-out.png";
+import imgPortrait from "../../Assets/gimp/portrait.jpg";
+import imgRainStorm from "../../Assets/gimp/rain-storm.png";
+import imgRain from "../../Assets/gimp/rain.jpg";
+import imgRealEstate2 from "../../Assets/gimp/real-estate-logo-2.png";
+import imgRealEstate from "../../Assets/gimp/real-estate-logo.png";
+import imgRealShadow from "../../Assets/gimp/real-shadow.png";
+import imgSakaMusiala from "../../Assets/gimp/saka-musiala.png";
+import imgSakaSmith from "../../Assets/gimp/saka-smith.webp";
+import imgShadow from "../../Assets/gimp/shadow.jpg";
+import imgSKullFace from "../../Assets/gimp/skull-face.webp";
+import imgSliced from "../../Assets/gimp/sliced.jpg";
+import imgStitch from "../../Assets/gimp/stitch.jpg";
+import imgTextAroundCIrcle from "../../Assets/gimp/text-around-circle.png";
+import imgVignette from "../../Assets/gimp/vignette.jpg";
+import imgWhitenTeeth from "../../Assets/gimp/whiten-teeth.webp";
+import imgYellowTeeth from "../../Assets/gimp/yellow-teeth.webp";
+
+
+
 function Gimp(){
   return(
     <Container fluid className="project-section">
       <Particle />
       <Container>
-        <div>
-          <h1 style={{ fontSize: "2.1em", paddingBottom: "10px" }}>Gimp Projects</h1>
-          <div className="d-flex">
-            <img className="col-md-3" src="../../Assets/Projects/pool_optimized.png" alt="gimp project"/>
+        <div className="min-h-screen">
+          <h1 style={{ fontSize: "2.1em", paddingBottom: "20px", color: '#fff' }}><strong className="purple">Gimp</strong> Projects</h1>
+          <div className="d-flex flex-wrap">
+            <img className="gimp-image col-md-3" src={img3DEffects} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgGlow} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgAntler} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgSmith} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgBrushed} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgBulb} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgCircuit} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgSKin} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgCubic} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgCurves} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgCylinderical} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgDayToNight} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgDoubleExposure} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgDualLight} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgDuotone} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgExplode} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgRealEstate} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgRealEstate2} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgFaceFlag} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgFaceSwap} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgFinal} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgFoggyWindow} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgGacLogo} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgGlitch} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgGlassSphere} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgGlossy} alt="gimp project"/><img className="gimp-image col-md-3" src={img3DEffects} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgPoolLOgo} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgGoldenEffect} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgHandToLogo} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgHeartCloud} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgInterlocking} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgLiquidDispersion} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgLogoHigh} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgMosaic} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgPopOut} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgOrangeGirl} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgOrthon} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgPGSLogo} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgPhoto} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgNeon} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgPortrait} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgRain} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgRainStorm} alt="gimp project"/><img className="gimp-image col-md-3" src={img3DEffects} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgRealShadow} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgSakaMusiala} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgShadow} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgSKullFace} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgSakaSmith} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgSliced} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgStitch} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgTextAroundCIrcle} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgVignette} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgWhitenTeeth} alt="gimp project"/>
+            <img className="gimp-image col-md-3" src={imgYellowTeeth} alt="gimp project"/>
           </div>
         </div>
     </Container>
