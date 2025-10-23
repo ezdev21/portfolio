ab25435d6bfb07569551cc28560a2505678eb59b
ezdev21 <ezradev21@gmail.com>
2025-10-23 15:49:46 -0700

    project image

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -6,7 +6,7 @@ import { BsGithub } from "react-icons/bs";
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" loading="lazy"/>
+      <Card.Img variant="top" src={props.imgPath} alt="card-img" height="200px" loading="lazy" style={{objectFit: 'cover'}}/>
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "left", fontWeight: '300', fontSize: '16px' }}>
