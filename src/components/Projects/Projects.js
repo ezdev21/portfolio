6b681b2ad1abb62ece2c269876282998a66ee991
ezdev21 <ezradev21@gmail.com>
2025-10-23 15:15:41 -0700

    project title font

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -16,7 +16,7 @@ function Projects() {
         <h1 className="project-heading">
           My Recent <strong className="purple">Works </strong>
         </h1>
-        <p style={{ color: "white" }}>
+        <p style={{ color: "white", fontSize: '21px', fontWeight: '100' }}>
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
