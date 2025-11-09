f3d6b9a8d2c2f139c4237ffcec2bae8d3a06e872
ezdev21 <ezradev21@gmail.com>
2025-11-09 15:13:32 -0800

    navbar brand

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/logo.png";
 import { Link } from "react-router-dom";
 import {
   AiOutlineHome,
@@ -36,18 +35,17 @@ function NavBar() {
       className={navColour ? "sticky" : "navbar"}
     >
       <Container>
-        <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
-        </Navbar.Brand>
+        <Nav.Link as={Link} to="/" className="">
+          <h1
+          style={{color: '#c770f0', fontWeight: '900', fontFamily: 'Azeret Mono'}}
+          >&lt;E/&gt;</h1>
+        </Nav.Link>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
           onClick={() => {
             updateExpanded(expand ? false : "expanded");
           }}
         >
-          <span></span>
-          <span></span>
-          <span></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav activeKey={activeKey} onSelect={(selectedKey) => setActiveKey(selectedKey)} className="ms-auto" defaultActiveKey="#home">
