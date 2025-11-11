c5c1562511762388785c1de3832505a11ecea95e
ezdev21 <ezradev21@gmail.com>
2025-11-11 12:12:52 -0800

    footer design improved

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,18 +9,18 @@ function Footer() {
   let date = new Date();
   let year = date.getFullYear();
   return (
-    <div className="bg-[#0A0416] py-[30px] px-4 md:px-10 bottom-0">
-      <div className="md:flex items-end">
-        <div className="md:w-1/2 md:text-center">
+    <div className="bg-[#0A0416] py-3 md:py-7 px-4 md:px-13 bottom-0">
+      <div className="flex flex-col md:flex-row md:items-end">
+        <div className="order-1 md:w-1/2 md:text-center">
           <div className="text-white md:flex flex-col items-start">
-            <h3 className="text-white font-medium pb-2 text-2xl">Get in touch!</h3>
-            <h6 className="text-start text-sm md:text-sm font-light">My inbox is always open. Whether you have a question or just want to say hello, I will try my best to get back to you!</h6>
+            <h3 className="text-white font-medium text-2xl">Get in touch!</h3>
+            <h6 className="text-start text-sm md:text-sm font-light py-2">My inbox is always open. Whether you have a question or just want to say hello, I will try my best to get back to you!</h6>
           </div>
         </div>
-        <div className="text-white md:w-1/4 md:text-center text-xs md:text-sm py-3">
+        <div className="order-3 md:order-2 text-white md:w-1/4 md:text-center text-sm md:text-sm py-2">
           <h6>Copyright © {year} Ezra F.</h6>
         </div>
-        <div className="md:w-1/4 z-1 text-center">
+        <div className="order-2 md:order-3 md:w-1/4 z-1 text-center">
           <ul className="my-[0.5em] p-0 flex justify-start md:justify-end">
             <li className="inline-block md:px-[15px]">
               <a
