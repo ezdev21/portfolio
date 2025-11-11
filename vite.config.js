e4d96b4ffd2146de25d22b2eb7b1550129161410
ezdev21 <ezradev21@gmail.com>
2025-11-10 23:18:41 -0800

    config

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     alias: {
       '@components': path.resolve(__dirname, './src/components'),
       '@pages': path.resolve(__dirname, './src/pages'),
-      '@assets': path.resolve(__dirname, './src/assets'),
+      '@assets': path.resolve(__dirname, './src/Assets'),
     },
   },
 })
