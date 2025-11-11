8b9f0d8ee9340ab4aa874e7869485ca9a3a503e2
ezdev21 <ezradev21@gmail.com>
2025-11-10 17:59:10 -0800

    vercel bug

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
       '@components': path.resolve(__dirname, './src/components'),
       '@pages': path.resolve(__dirname, './src/pages'),
       '@assets': path.resolve(__dirname, './src/assets'),
