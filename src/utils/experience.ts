fe1daae521967b56419db120e02a82773c3d4b38
ezdev21 <ezradev21@gmail.com>
2025-11-17 00:31:18 +0300

    update resume

--- src/utils/experience.ts
+++ src/utils/experience.ts
@@ -11,12 +11,12 @@ const experiences:Experience[] = [
     position: "Full-stack Developer",
     date: "Feb 2025 - Aug 2025",
     responsibilities: [
-      "Complete detailed programming and development tasks for front-end as well as challenging back-end servercode.",
-      "Completed several small tasks with deadlines, which let me adapt to working under a tight deadline and learnhow to divide/prioritize tasks",
-      "Designed and implemented mobile-first, user-friendly UI designs for responsible and accessible websites",
-      "Orchestrate efficient large-scale software deployments, including testing features and correcting code.",
+      "Collaborated with cross-functional teams to design, develop, and deploy governmental projects.",
+      "Completed several small tasks with deadlines, which let me adapt to working under a tight deadline and learn how to divide/prioritize tasks.",
+      "Designed and implemented mobile-first, user-friendly UI designs for responsible and accessible websites.",
+      "Manage large-scale software deployments including testing features and fixing code issues.",
       "Propose improvements to code organization to improve code quality and overall performance.",
-      "Meet with the project manager and team members to provide detailed project reports and milestone updates" 
+      "Debugged complex issues across the UI, API, and database using logs and dev-tools.",
     ]
   },
   {
@@ -24,11 +24,11 @@ const experiences:Experience[] = [
     position: "Software Developer",
     date: "Sep 2024 - Feb 2025",
     responsibilities: [
-      "Contributed ideas and suggestions in team meetings and delivered updates on deadlines, designs, and enhancements.",
-      "Involved in all phases of the Software Development Life Cycle, including user interface, API development, deployment, and mobile application development",
-      "Develop an easy-to-use and optimized administration dashboard for administrators.",
-      "Provided a documented API for the mobile application team to use.",
-      "Completed several small tasks with deadlines which let me adapt to working under a tight deadline and how todivide/prioritize tasks"
+      "Wrote easy-to-understand, documented API for the mobile app using Postman Collections.",
+      "Involved in all phases of the Software Development Life Cycle, including user interface, API development, deployment, and mobile application development.",
+      "Develop an easy-to-use and optimized administration dashboard for administrators using React and Laravel.",
+      "Completed several small tasks with deadlines which allowed me adapt to working under a tight deadline and how to divide/prioritize tasks.",
+      "Launched cross platform mobile application of Biritu using React Native.",
     ]
   },
   {
@@ -36,33 +36,35 @@ const experiences:Experience[] = [
     position: "Full-stack Developer",
     date: "Jun 2023 - Sep 2024",
     responsibilities: [
-      "Built integrations for appropriate map services, SMS services, and payment systems.",
-      "Coached junior developers and interns.",
-      "Wrote easy-to-understand, structured documentation for the open-source community with easy-to-use toolssuch as Postman Collections",
-      "Worked directly with the headquarters located in Nairobi, Kenya, using remote development technologies",
-      "Improved product speed and quality by revising and refactoring code written by less experienced developers",
-      "Orchestrate efficient large-scale software deployments, including testing features and correcting code",
+      "Built integrations for appropriate map services, SMS services, and payment gateways.",
+      "Worked directly with the headquarters located in Nairobi, Kenya, using remote development technologies.",
+      "Integrated core features like achievements, spin and win lottery system, backup system.",
+      "Improved product speed and quality by revising and refactoring code written by less experienced developers.",
+      "managed large-scale software deployments to AWS and GCP, including testing features and correcting code.",
+      "Mentored internships, providing guidance on coding practices, design, and API development.",
     ]
   },
   {
     company: "MADOT SOLUTIONS",
-    position: "Full-stack Developer",
+    position: "Junior Full-stack Developer",
     date: "Dec 2021 - Sep 2022",
     responsibilities: [
-      "Led the development and architectural design of a complex web application implementing scalable and maintainable backend architecture",
-      "Actively contributed to the company’s engineering culture by sharing knowledge, delivering tech talks during weekly dev-team retrospectives, and organizing internal workshops or training sessions",
-      "Collaborated with cross-functional teams to design, develop, and deploy an app with integrations on various platforms, resulting in a successful launch of the product",
-      "Contributed to the development of logistics system and health management system, with development, database design, UI improvement, and requirement gathering"
+      "Debugged complex issues across the UI, API, and database layers using logs, monitoring tools, and dev-tools.",
+      "Collaborated with cross-functional teams to design, develop, and deploy an app to the cloud.",
+      "Contributed to the development of Logistics System and Lideta Health Science College website, with UI improvement and API development.",
+      "Develop and maintain user interfaces using modern frameworks like React.js, Vue.js, Bootstrap and Tailwindcss.",
+      "Designed and optimized database schemas, queries, and data models."
     ]
   },
   {
     company: "BOTAYE DATA SCIENCE",
-    position: "Data Science Intern",
-    date: "Jun 2021 - Nov 2021",
+    position: "Junior Web Developer",
+    date: "Aug 2021 - Dec 2021",
     responsibilities: [
-      "Prepared reports on programming project specifications, activities, or status",
-      "Directing software programming and documentation development",
-      "Implemented UI designs for responsive and accessible websites"
+      "Gathered, documented, and maintained software requirements throughout the project lifecycle.",
+      "Converted design prototypes created in Figma into functional, dynamic web components using React andTailwindcss.",
+      "Designed and implemented backend features, including authentication, authorization and database architecture of application.",
+      "Diagnosed and resolved application issues by analyzing logs, debugging components, and identifying root causes."
     ]
   },
   // {
