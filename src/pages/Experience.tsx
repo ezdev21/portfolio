18c35408fc689a40d793cc61f13e38020625c624
ezdev21 <ezradev21@gmail.com>
2025-11-16 16:51:48 +0300

    experience code refactor

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import experiences from "../utils/experience";
 
 const sectionVariants = {
   initial: {opacity: 0.9,scale: 0.9},
@@ -45,87 +46,25 @@ export default function Experience() {
               transition={{ type: 'spring', stiffness: 300}}
             >Professional <strong className="text-primary pb-5">Experience</strong></motion.h1>
           </div>
-          <motion.div variants={variants} initial="initial" whileInView="animate" viewport={{ once: true }}  whileHover="hover" whileTap="tap" exit="exit">
-            <div className="p-5 my-10 card rounded-lg">
-              <div>
-                <h2 className="font-bold text-2xl md:text-4xl my-2">HADDIS ART</h2>
-                <h6 className="my-2 max-md:text-md">Full-stack Developer Feb 2025 - Aug 2025</h6>
-                <hr className="my-3 border-gray-300 border-t-3 opacity-25"/>
-                <ul className="p-0 font-extralight md:font-light text-sm md:text-lg text-left list-disc">
-                  <li>Complete detailed programming and development tasks for front-end as well as challenging back-end servercode.                </li>
-                  <li>Completed several small tasks with deadlines, which let me adapt to working under a tight deadline and learnhow to divide/prioritize tasks</li>
-                  <li>Designed and implemented mobile-first, user-friendly UI designs for responsible and accessible websites</li>
-                  <li>Orchestrate efficient large-scale software deployments, including testing features and correcting code.</li>
-                  <li>Propose improvements to code organization to improve code quality and overall performance.</li>
-                  <li>Meet with the project manager and team members to provide detailed project reports and milestone updates</li>
-              </ul>
-              </div>
-            </div>
-          </motion.div>
-          <motion.div variants={variants} initial="initial" whileInView="animate" viewport={{ once: true }}  whileHover="hover" whileTap="tap" exit="exit">
-            <div className="p-5 my-10 card rounded-lg">
-              <div>
-                <h2 className="font-bold text-2xl md:text-4xl my-2">BIRITU DIGITAL SAVING AND COOP</h2>
-                <h6 className="my-2">Software Developer Sep 2024 - Feb2025</h6>
-                <hr className="my-3 border-gray-300 border-t-3 opacity-25"/>
-                <ul className="p-0 font-extralight md:font-light text-sm md:text-lg text-left list-disc">
-                  <li>Contributed ideas and suggestions in team meetings and delivered updates on deadlines, designs, and enhancements.</li>
-                  <li>Involved in all phases of the Software Development Life Cycle, including user interface, API development, deployment, and mobile application development</li>
-                  <li>Develop an easy-to-use and optimized administration dashboard for administrators.</li>
-                  <li>Provided a documented API for the mobile application team to use.</li>
-                  <li> Completed several small tasks with deadlines which let me adapt to working under a tight deadline and how todivide/prioritize tasks</li>
-                </ul>
-              </div>
-            </div>
-          </motion.div>
-          <motion.div variants={variants} initial="initial" whileInView="animate" viewport={{ once: true }}   whileHover="hover" whileTap="tap" exit="exit">
-            <div className="p-5 my-10 card rounded-lg">
-              <div>
-                <h2 className="font-bold text-2xl md:text-4xl my-2">POOL TAXI</h2>
-                <h6 className="my-2">Full-stack Developer Jun 2023 - Sep 2024</h6>
-                <hr className="my-3 border-gray-300 border-t-3 opacity-25"/>
-                <ul className="p-0 font-extralight md:font-light text-sm md:text-lg text-left list-disc ">
-                  <li>Built integrations for appropriate map services, SMS services, and payment systems.</li>
-                  <li>Coached junior developers and interns.</li>
-                  <li>Wrote easy-to-understand, structured documentation for the open-source community with easy-to-use toolssuch as Postman Collections</li>
-                  <li>Worked directly with the headquarters located in Nairobi, Kenya, using remote development technologies</li>
-                  <li>Improved product speed and quality by revising and refactoring code written by less experienced developers</li>
-                  <li>Orchestrate efficient large-scale software deployments, including testing features and correcting code</li>
-                </ul>
-              </div>
-            </div>
-          </motion.div>
-          <motion.div variants={variants} initial="initial" whileInView="animate" viewport={{ once: true }}   whileHover="hover" whileTap="tap" exit="exit">
-            <div className="p-5 my-10 card rounded-lg">
-              <div>
-                <h2 className="font-bold text-2xl md:text-4xl my-2">MADOT SOLUTIONS</h2>
-                <h6 className="my-2">Full-stack Developer Dec 2021 - Sep 2022</h6>
-                <hr className="my-3 border-gray-300 border-t-3 opacity-25"/>
-                <ul className="p-0 font-extralight md:font-light text-sm md:text-lg text-left my-5 list-disc">
-                  <li>Led the development and architectural design of a complex web application implementing scalable and maintainable backend architecture</li>
-                  <li>Actively contributed to the company’s engineering culture by sharing knowledge, delivering tech talks during weekly dev-team retrospectives, and organizing internal workshops or training sessions</li>
-                  <li>Collaborated with cross-functional teams to design, develop, and deploy an app with integrations on various platforms, resulting in a successful launch of the product</li>
-                  <li>Contributed to the development of logistics system and health management system, with development, database design, UI improvement, and requirement gathering</li>
-                </ul>
-              </div>
-            </div>
-          </motion.div>
-          <motion.div variants={variants} initial="initial" whileInView="animate" viewport={{ once: true }}   whileHover="hover" whileTap="tap" exit="exit">
-            <div className="p-5 my-10 card rounded-lg">
-              <div>
-                <h2 className="font-bold text-2xl md:text-4xl my-2">BOTAYE DATA SCIENCE</h2>
-                <h6 className="my-2">Full-stack Developer Aug 2021 - Dec 2021</h6>
-                <hr className="my-3 border-gray-300 border-t-3 opacity-25"/>
-                <ul className="p-0 font-extralight md:font-light text-sm md:text-lg text-left list-disc">
-                  <li>Prepared reports on programming project specifications, activities, or status</li>
-                  <li>Conferred with project managers to obtain information on limitations or capabilities</li>
-                  <li>Directing software programming and documentation development</li>
-                  <li>Implemented UI designs for responsive and accessible websites</li>
-                </ul>
-              </div>
-            </div>
-          </motion.div>
+          <div>
+            {experiences.map((experience, index) => (
+              <motion.div variants={variants} initial="initial" whileInView="animate" viewport={{ once: true }}   whileHover="hover" whileTap="tap" exit="exit">
+                <div className="p-5 my-10 card rounded-lg">
+                  <div>
+                    <h2 className="font-bold text-2xl md:text-4xl my-2">{experience.company}</h2>
+                    <h6 className="my-2">{experience.position} {experience.date}</h6>
+                    <hr className="my-3 border-gray-300 border-t-3 opacity-25"/>
+                    <ul className="p-0 font-extralight md:font-light text-sm md:text-lg text-left list-disc ">
+                      {experience.responsibilities.map((responsibility, idx) => (
+                        <li key={idx} className="mb-2">{responsibility}</li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+            </motion.div>
+            ))}
         </div>
+      </div>
       </div>
     </motion.section>
   );
---
+++ src/utils/experience.ts
@@ -0,0 +1,83 @@
+interface Experience {
+  company: string;
+  position: string;
+  date: string;
+  responsibilities: string[];
+}
+
+const experiences:Experience[] = [
+  {
+    company: "HADDIS ART",
+    position: "Full-stack Developer",
+    date: "Feb 2025 - Aug 2025",
+    responsibilities: [
+      "Complete detailed programming and development tasks for front-end as well as challenging back-end servercode.",
+      "Completed several small tasks with deadlines, which let me adapt to working under a tight deadline and learnhow to divide/prioritize tasks",
+      "Designed and implemented mobile-first, user-friendly UI designs for responsible and accessible websites",
+      "Orchestrate efficient large-scale software deployments, including testing features and correcting code.",
+      "Propose improvements to code organization to improve code quality and overall performance.",
+      "Meet with the project manager and team members to provide detailed project reports and milestone updates" 
+    ]
+  },
+  {
+    company: "BIRITU DIGITAL SAVING AND COOP",
+    position: "Software Developer",
+    date: "Sep 2024 - Feb 2025",
+    responsibilities: [
+      "Contributed ideas and suggestions in team meetings and delivered updates on deadlines, designs, and enhancements.",
+      "Involved in all phases of the Software Development Life Cycle, including user interface, API development, deployment, and mobile application development",
+      "Develop an easy-to-use and optimized administration dashboard for administrators.",
+      "Provided a documented API for the mobile application team to use.",
+      "Completed several small tasks with deadlines which let me adapt to working under a tight deadline and how todivide/prioritize tasks"
+    ]
+  },
+  {
+    company: "POOL TAXI",
+    position: "Full-stack Developer",
+    date: "Jun 2023 - Sep 2024",
+    responsibilities: [
+      "Built integrations for appropriate map services, SMS services, and payment systems.",
+      "Coached junior developers and interns.",
+      "Wrote easy-to-understand, structured documentation for the open-source community with easy-to-use toolssuch as Postman Collections",
+      "Worked directly with the headquarters located in Nairobi, Kenya, using remote development technologies",
+      "Improved product speed and quality by revising and refactoring code written by less experienced developers",
+      "Orchestrate efficient large-scale software deployments, including testing features and correcting code",
+    ]
+  },
+  {
+    company: "MADOT SOLUTIONS",
+    position: "Full-stack Developer",
+    date: "Dec 2021 - Sep 2022",
+    responsibilities: [
+      "Led the development and architectural design of a complex web application implementing scalable and maintainable backend architecture",
+      "Actively contributed to the company’s engineering culture by sharing knowledge, delivering tech talks during weekly dev-team retrospectives, and organizing internal workshops or training sessions",
+      "Collaborated with cross-functional teams to design, develop, and deploy an app with integrations on various platforms, resulting in a successful launch of the product",
+      "Contributed to the development of logistics system and health management system, with development, database design, UI improvement, and requirement gathering"
+    ]
+  },
+  {
+    company: "BOTAYE DATA SCIENCE",
+    position: "Data Science Intern",
+    date: "Jun 2021 - Nov 2021",
+    responsibilities: [
+      "Prepared reports on programming project specifications, activities, or status",
+      "Directing software programming and documentation development",
+      "Implemented UI designs for responsive and accessible websites"
+    ]
+  },
+  // {
+  //   company: "FREELANCE PROJECTS",
+  //   position: "Freelance Developer",
+  //   date: "Jun 2022 - Present",
+  //   responsibilities: [
+  //     "Worked with clients to understand their requirements and deliver customized solutions.",
+  //     "Managed multiple projects simultaneously, ensuring timely delivery and quality output.",
+  //     "Provided ongoing support and maintenance for deployed applications.",
+  //     "Built strong relationships with clients, resulting in repeat business and referrals.",
+  //     "Utilized a variety of technologies and tools to meet diverse project needs.",
+  //     "Continuously improved development processes to enhance efficiency and effectiveness."
+  //   ]
+  // }
+]
+
+export default experiences;
