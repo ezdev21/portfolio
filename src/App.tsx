84a36753654c66690406b97a216af2b21c7c4151
ezdev21 <ezradev21@gmail.com>
2025-12-01 00:44:50 +0300

    remove scroll preservation on routing

--- src/App.tsx
+++ src/App.tsx
@@ -23,9 +23,11 @@ function App() {
     const timer = setTimeout(() => {
       upadateLoad(false);
     }, 1200);
+    
+    window.scrollTo(0, 0);
 
     return () => clearTimeout(timer);
-  }, []);
+  }, [location.pathname]);
 
   return (
     <div>
