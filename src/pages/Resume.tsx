a884486d3e1d53d0e3493e89a1791d1d3831d72e
ezdev21 <ezradev21@gmail.com>
2026-03-02 09:49:03 +0300

    vercel bug fixed

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import pdf from "@/assets/RESUME_EZRA_UPDATED.pdf";
 import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
 import { motion } from "framer-motion";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
