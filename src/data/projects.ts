9ead4bc5427df0c35da13c90142d5ba324d351b5
ezdev21 <ezradev21@gmail.com>
2026-03-04 14:53:02 +0300

    projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -21,14 +21,25 @@ export interface ProjectProps {
 
 export const projects: ProjectProps[] = [
   {
-    imgPath: haddisArt,
+    imgPath: video_sharing,
     isBlog: false,
-    title: "Haddis Art",
+    title: "Video sharing",
     description:
-      "Haddis Art is an online art gallery that showcases and sells artwork from various artists. It provides a platform for artists to display their work and for art enthusiasts to discover and purchase unique pieces.",
+      "video streaming and sharing platform where users can watch, like, and comment on videos, subscribe to channels, and receive personalized recommendations. Users can also create their own channels, upload and organize videos, and engage with a community through comments and interactions. The application supports video streaming, content management, and user-driven engagement features.",
     demoLink: "",
-    ghLink: "",
+    ghLink: "https://github.com/ezdev21/video-sharing",
     order: 1,
+    skills: [],
+  },
+  {
+    imgPath: ecommerce,
+    isBlog: false,
+    title: "Ecommerce",
+    description:
+      "An e-commerce web application that allows users to browse products, add items to a cart, and place orders. It provides sellers with tools to manage products, inventory, orders, and customers, delivering a complete online shopping experience.",
+    demoLink: "",
+    ghLink: "https://github.com/ezdev21/ecommerce",
+    order: 2,
     skills: [],
   },
   {
@@ -39,7 +50,7 @@ export const projects: ProjectProps[] = [
       "A Digital Savings Account (Biritu) is an electronic version of a regular Savings Account. With a paperless, quick and secure account opening process",
     demoLink: "",
     ghLink: "",
-    order: 2,
+    order: 3,
     skills: [],
   },
   {
@@ -50,7 +61,7 @@ export const projects: ProjectProps[] = [
       "Pool Taxi is a technology-based company that works in providing cost effective and technologically advanced platforms to the society. It offers transportation services to people, organizations, and institutions.",
     demoLink: "",
     ghLink: "",
-    order: 3,
+    order: 1,
     skills: [],
   },
   {
@@ -65,17 +76,6 @@ export const projects: ProjectProps[] = [
     skills: [],
   },
   {
-    imgPath: ecommerce,
-    isBlog: false,
-    title: "Ecommerce",
-    description:
-      "An e-commerce web application that allows users to browse products, add items to a cart, and place orders. It provides sellers with tools to manage products, inventory, orders, and customers, delivering a complete online shopping experience.",
-    demoLink: "",
-    ghLink: "https://github.com/ezdev21/ecommerce",
-    order: 3,
-    skills: [],
-  },
-  {
     imgPath: madot,
     isBlog: false,
     title: "Madot Solutions",
@@ -83,18 +83,7 @@ export const projects: ProjectProps[] = [
       "The company's mission is to promote problem-solving within our organization; produce, deliver, and support technology to make it more accessible to our customers. Increasing the capital of the organization, creating job opportunities, playing a significant role in the economic development of our country.",
     demoLink: "",
     ghLink: "",
-    order: 1,
-    skills: [],
-  },
-  {
-    imgPath: video_sharing,
-    isBlog: false,
-    title: "Video sharing",
-    description:
-      "video streaming and sharing platform where users can watch, like, and comment on videos, subscribe to channels, and receive personalized recommendations. Users can also create their own channels, upload and organize videos, and engage with a community through comments and interactions. The application supports video streaming, content management, and user-driven engagement features.",
-    demoLink: "",
-    ghLink: "https://github.com/ezdev21/video-sharing",
-    order: 2,
+    order: 3,
     skills: [],
   },
   {
@@ -105,7 +94,7 @@ export const projects: ProjectProps[] = [
       "Union academy dedicated to holistic education, fostering innovation, nurturing talents, and cultivating a vibrant learning community for future leaders.",
     demoLink: "",
     ghLink: "",
-    order: 3,
+    order: 1,
     skills: [],
   },
   {
@@ -116,7 +105,7 @@ export const projects: ProjectProps[] = [
       "Origin land general trading PLC is registered in Ethiopia to engage in Agricultural products export mainly Green Arabica Coffee, Roasted Coffee, Minerals, oil seeds, pulses, spices industrial and chemical products import to Ethiopia.",
     demoLink: "",
     ghLink: "",
-    order: 1,
+    order: 2,
     skills: [],
   },
 ];
